f2c4ed24812d67576af22bd90950dfc4408c5312
Bon Axl Feeser <bonaxl015@gmail.com>
2022-10-24 23:57:19 +0800

    (feat/button) Added new set of stories for button types

---
+++ src/components/Button/stories/button-basic.stories.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "../Button";
+
+export default {
+  title: "CoffeeUI/Button/Basic",
+  component: Button,
+  argTypes: {},
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "Primary",
+  variant: "primary",
+  onClick: () => console.log('Primary')
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  label: "Success",
+  variant: "success",
+  onClick: () => console.log('Success')
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  label: "Warning",
+  variant: "warning",
+  onClick: () => console.log('Warning')
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  label: "Danger",
+  variant: "danger",
+  onClick: () => console.log('Danger')
+};
+
+export const Info = Template.bind({});
+Info.args = {
+  label: "Info",
+  variant: "info",
+  onClick: () => console.log('Info')
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  label: "Text",
+  variant: "text",
+  onClick: () => console.log('Text')
+};
--- src/components/Button/stories/button-plain.stories.tsx
+++ src/components/Button/stories/button-plain.stories.tsx
@@ -1,63 +1,59 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Button } from "./Button";
+import { Button } from "../Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "CoffeeUI/Button",
+  title: "CoffeeUI/Button/Plain",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = Template.bind({});
 Primary.args = {
   label: "Primary",
-  size: "medium",
   variant: "primary",
-  disabled: false,
+  plain: true,
+  onClick: () => console.log('Primary')
 };
 
 export const Success = Template.bind({});
 Success.args = {
   label: "Success",
-  size: "medium",
   variant: "success",
-  disabled: false,
+  plain: true,
+  onClick: () => console.log('Success')
 };
 
 export const Warning = Template.bind({});
 Warning.args = {
   label: "Warning",
-  size: "medium",
   variant: "warning",
-  disabled: false,
+  plain: true,
+  onClick: () => console.log('Warning')
 };
 
 export const Danger = Template.bind({});
 Danger.args = {
   label: "Danger",
-  size: "medium",
   variant: "danger",
-  disabled: false,
+  plain: true,
+  onClick: () => console.log('Danger')
 };
 
 export const Info = Template.bind({});
 Info.args = {
   label: "Info",
-  size: "medium",
   variant: "info",
-  disabled: false,
+  plain: true,
+  onClick: () => console.log('Info')
 };
 
 export const Text = Template.bind({});
 Text.args = {
   label: "Text",
-  size: "medium",
   variant: "text",
-  disabled: false,
+  plain: true,
+  onClick: () => console.log('Text')
 };
